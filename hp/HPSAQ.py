1d58f41c3e4f8949ebba3df98e78fc7e6de053a4
aengusl <aenguslynch@gmail.com>
2024-01-12 00:25:38 +0000

    working task for harry potter short answer questions

---
+++ hp/HPSAQ.py
@@ -0,0 +1,237 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+import json
+import pandas as pd
+import openai
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+from tasks.task import Task
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+client = openai.Client(
+    organization='org-X6T6Ar6geRtOrQgQTQS3OUpw',
+)
+
+QA_TEMPLATE = """
+Question:
+{question}
+
+Answer:
+{answer}
+"""
+
+Q_TEMPLATE = """
+Question:
+{question}
+
+Answer:
+"""
+
+ZERO_SHOT_TEMPLATE = """
+I want you to answer the following question about Harry Potter and respond with an answer. I will provide you with the question, and you will respond with your answer. Your response should be a single sentence. I will now provide you with the question.
+{question}"""
+
+FEW_SHOT_TEMPLATE = """
+I want you to answer the following question about Harry Potter and respond with an answer. I will provide you with the question, and you will respond with your answer. Your response should be a single sentence. I will now provide you with the question.
+{few_shot_questions}
+{{question}}"""
+
+UNRELATED_FEW_SHOT_TEMPLATE = """
+I want you to answer the following question about Harry Potter and respond with an answer. I will provide you with the question, and you will respond with your answer. Your response should be a single sentence. I will now provide you with the question.
+
+Question:
+Which planet in our solar system is known as the Red Planet?
+
+Answer:
+Mars
+
+Question:
+Who wrote the play "Romeo and Juliet"?
+
+Answer:
+William Shakespeare
+
+Question:
+Name the Great Lakes of North America.
+
+Answer:
+Huron, Ontario, Michigan, Erie, Superior
+{question}"""
+
+EVAL_SYSTEM_MESSAGE = """
+I want you to make a judgement on the correctness of a given answer by a contestant. The question pertains to the Harry Potter universe. The contestant has been asked a question and has responded with their answer. Using the provided "Perfect Answer" and your own knowledge, you will decide whether the contestant's response is correct. Your output will be either `Y` or `N` for yes or no. You must not output anything else.
+"""
+EVAL_USER_MESSAGE = """
+Question: {question}
+Contestant's Answer: {answer}
+Perfect Answer: {perfect_answer}
+"""
+
+def generate_sentence(str, model, tokenizer, with_logprobs=False, max_new_tokens=20, top_tokens=5, show_token_strs=True):
+    tokenized_str = tokenizer(str, return_tensors="pt").input_ids.cuda()
+    start_len = tokenized_str.shape[1]
+    generated_output = model.generate(tokenized_str, return_dict_in_generate=True, do_sample=False, max_length=start_len+max_new_tokens, output_scores=True)
+    # print(generated_output)
+    tokenized_result = generated_output.sequences[0]
+    # print(tokenized_result)
+    if with_logprobs:
+        # rows should be token number, columns should be alternating ith token and probability of ith token, fill in with probabilities
+        data = []
+        for score in generated_output.scores:
+            # a tensor of logits, translate into probabilities
+            probs = torch.nn.functional.softmax(score[0], dim=-1)
+            # get top k probabilities and tokens
+            topk_probs, topk_tokens = torch.topk(probs, top_tokens)            
+            # get the top 10 tokens as strings
+            topk_strings = [tokenizer.decode(token) for token in topk_tokens]
+
+            row = {}
+            # fill in df
+            for i in range(top_tokens):
+                row[f'Token_{i+1}'] = topk_tokens[i].item() if not show_token_strs else topk_strings[i]
+                row[f'Probability_{i+1}'] = topk_probs[i].item()
+            data.append(row)
+        probs_df = pd.DataFrame(data)
+        
+        return tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(str, ""), probs_df
+    else:
+        return tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(str, "")
+    
+def clear_gpu(model):
+    model.cpu()
+    torch.cuda.empty_cache()
+
+def compare_responses(prompt, model1, model2, max_new_tokens=200, manage_gpu=False):
+    if manage_gpu:
+        clear_gpu(model1)
+        clear_gpu(model2)
+        model1.cuda()
+    model1_gen = generate_sentence(prompt, model1, max_new_tokens=max_new_tokens)
+    if manage_gpu:
+        clear_gpu(model1)
+        model2.cuda()
+    model2_gen = generate_sentence(prompt, model2, max_new_tokens=max_new_tokens)
+    if manage_gpu:
+        clear_gpu(model2)
+    return model1_gen, model2_gen
+
+def save_list_to_jsonl(path, list_to_save):
+    with open(path, 'w') as f:
+        for datapoint in list_to_save:
+            json.dump(datapoint, f)
+            f.write('\n')
+
+def get_model_grade(client, question, response, perfect_answer, model='gpt-3.5-turbo', max_tokens=1):
+
+    system_message = EVAL_SYSTEM_MESSAGE
+    user_message = EVAL_USER_MESSAGE.format(question=question, answer=response, perfect_answer=perfect_answer)
+
+    response = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": user_message},
+    ],
+    temperature=0,
+    seed=42,
+    )
+    return response.choices[0].message.content
+
+
+
+class HPSAQ(Task):
+
+    def __init__(self, dataset_path):
+
+        with open(dataset_path, 'r') as f:
+            lines = f.readlines()
+            self.raw_dataset = [json.loads(line) for line in lines]
+        for line in self.raw_dataset:
+            assert isinstance(line, dict), "Each line in the dataset should be a dictionary"
+            assert 'question' in line, "Key 'question' not found in the dictionary"
+            assert 'true_answer' in line, "Key 'true_answer' not found in the dictionary"
+        self.answered_dataset = []
+
+        # get first three questions from dataset and create the few-shot template
+        few_shot_questions = []
+        for question in self.raw_dataset[:3]:
+            few_shot_questions.append(QA_TEMPLATE.format(question=question['question'], answer=question['true_answer']))
+        few_shot_questions = '\n'.join(few_shot_questions)
+        self.few_shot_template = FEW_SHOT_TEMPLATE.format(few_shot_questions=few_shot_questions)
+
+    def generate_responses(self, model, tokenizer, save_path=None, eval_onthe_fly=True, question_types=None, eval_model=None):
+
+        self.answered_dataset = []
+
+        if question_types is None:
+            question_types = ['zero_shot', 'few_shot', 'unrelated_few_shot']
+        if isinstance(question_types, str):
+            question_types = [question_types]
+        for question_type in question_types:
+            assert question_type in ['zero_shot', 'few_shot', 'unrelated_few_shot'], f"Question type {question_type} not recognized"
+        
+        if eval_onthe_fly:
+            if eval_model is None:
+                eval_model = 'gpt-3.5-turbo'
+
+        if save_path is None:
+            exp_time = datetime.now().strftime("%a-%b%-d-%H%M")
+            os.makedirs('temp', exist_ok=True)
+            save_path = f'temp/{exp_time}.jsonl'
+
+        model.cuda()
+
+        for i, datapoint in enumerate(self.raw_dataset):
+
+            print(f"\nQuestion {i+1}/{len(self.raw_dataset)} -- Time: {datetime.now().strftime('%H:%M:%S')}")
+
+            results_dict = {
+                'raw_question': datapoint['question'],
+                'true_answer': datapoint['true_answer'],
+                }
+            for question_type in question_types:
+                if question_type == 'zero_shot':
+                    question, answer = datapoint['question'], datapoint['true_answer']
+                elif question_type == 'few_shot':
+                    if i < 3:
+                        continue # skip first three questions because they are used to create the few-shot template
+                    question, answer = self.few_shot_template.format(question=datapoint['question']), datapoint['true_answer']
+                elif question_type == 'unrelated_few_shot':
+                    question, answer = UNRELATED_FEW_SHOT_TEMPLATE.format(question=datapoint['question']), datapoint['true_answer']
+                else:
+                    raise ValueError(f"Question type {question_type} not recognized")
+                # generate response
+                response = generate_sentence(question, model, tokenizer, max_new_tokens=20).split('\nQuestion')[0].strip()
+                # add response to results dict
+                results_dict[question_type] = {
+                    'question': question,
+                    'response': response,
+                }
+
+                # run evaluation
+                if eval_onthe_fly:
+                    model_grade = get_model_grade(client, question, response, answer, model=eval_model, max_tokens=1)
+                    results_dict[question_type]['model_grade'] = model_grade
+                
+            self.answered_dataset.append(results_dict)
+                
+            save_list_to_jsonl(save_path, self.answered_dataset)
+            print(f"Saved results to {save_path}")
+                
+
+if __name__ == '__main__':
+    # load model
+    hp_model = AutoModelForCausalLM.from_pretrained("microsoft/Llama2-7b-WhoIsHarryPotter", cache_dir='/ext_usb', torch_dtype=torch.bfloat16).cuda()
+    # llama_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", cache_dir='/ext_usb', torch_dtype=torch.bfloat16)
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/Llama2-7b-WhoIsHarryPotter")
+    tokenizer.pad_token = tokenizer.eos_token
+
+
+    # load dataset
+    dataset_path = '/ext_usb/Desktop/mats/hp-unlrn/aengus_testing/datasets/harry_potter_trivia_502_v2.jsonl'
+    hp_task = HPSAQ(dataset_path)
+    exp_time = datetime.now().strftime("%a-%b%-d-%H%M")
+    save_path = f'/ext_usb/Desktop/mats/hp-unlrn/aengus_testing/garbage/{exp_time}.jsonl'
+    hp_task.generate_responses(hp_model, tokenizer, save_path=save_path, eval_onthe_fly=True, eval_model='gpt-3.5-turbo')
