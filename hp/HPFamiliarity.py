1061060b3b9fb7cfe258a4c82e82e84fcdade1d0
Aengus <aenguslynch@gmail.com>
2024-02-01 21:56:49 +0000

    minor

--- hp/HPFamiliarity.py
+++ hp/HPFamiliarity.py
@@ -15,7 +15,7 @@ try:
     load_dotenv()
     openai.api_key = os.getenv("OPENAI_API_KEY")
     client = openai.Client(
-        organization='org-X6T6Ar6geRtOrQgQTQS3OUpw',
+        organization="org-X6T6Ar6geRtOrQgQTQS3OUpw",
     )
 except:
     print("OpenAI API key not found, will not be able to run evaluations on HPSAQ Task")
@@ -46,35 +46,47 @@ Model completion: {answer}
 
 
 def save_list_to_jsonl(path, list_to_save):
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         for datapoint in list_to_save:
             json.dump(datapoint, f)
-            f.write('\n')
+            f.write("\n")
 
-def get_model_grade(client, question, response, references, model='gpt-3.5-turbo', max_tokens=1, seed=42, logit_bias={15: 100, 16: 100, 17: 100, 18: 100}):
+
+def get_model_grade(
+    client,
+    question,
+    response,
+    references,
+    model="gpt-3.5-turbo",
+    max_tokens=1,
+    seed=42,
+    logit_bias={15: 100, 16: 100, 17: 100, 18: 100},
+):
     # boost logit bias for tokens 15-18, corresponding to 0, 1, 2, 3
 
-    system_message = EVAL_SYSTEM_MESSAGE.format(references=references, prompt=question, completion=response)
+    system_message = EVAL_SYSTEM_MESSAGE.format(
+        references=references, prompt=question, completion=response
+    )
     # user_message = EVAL_USER_MESSAGE.format(question=question, answer=response)
     print(system_message)
 
     if logit_bias is None:
         logit_bias = {}
     gpt_answer = client.chat.completions.create(
-    model=model,
-    messages=[
-        {"role": "system", "content": system_message},
-        # {"role": "user", "content": user_message},
-    ]
-    temperature=0,
-    seed=seed,
-    max_tokens=max_tokens,
-    logit_bias=logit_bias,
+        model=model,
+        messages=[
+            {"role": "system", "content": system_message},
+            # {"role": "user", "content": user_message},
+        ],
+        temperature=0,
+        seed=seed,
+        max_tokens=max_tokens,
+        logit_bias=logit_bias,
     )
 
     gpt_response = gpt_answer.choices[0].message.content
     try:
-        gpt_response = int(gpt_response.split('MODEL_FAMILIARITY: ')[-1].split('/')[0])
+        gpt_response = int(gpt_response.split("MODEL_FAMILIARITY: ")[-1].split("/")[0])
     except:
         print("Error in getting model grade, returning -100")
         gpt_response = -100
@@ -92,50 +104,86 @@ class HPCompletionsFamiliarity(Task):
         4. Completions that reveal no familiarity
 
     """
-    def generate_sentence(self, str, model, tokenizer, with_logprobs=False, max_new_tokens=20, top_tokens=5, show_token_strs=True, temperature=0, include_input=True):
+
+    def generate_sentence(
+        self,
+        str,
+        model,
+        tokenizer,
+        with_logprobs=False,
+        max_new_tokens=20,
+        top_tokens=5,
+        show_token_strs=True,
+        temperature=0,
+        include_input=True,
+    ):
         tokenized_str = tokenizer(str, return_tensors="pt").input_ids.cuda()
         start_len = tokenized_str.shape[1]
 
         # if hasattr(model, 'generate'): # should be huggingface model
         try:
-            generated_output = model.generate(tokenized_str, return_dict_in_generate=True, do_sample=False, max_length=start_len+max_new_tokens, output_scores=True, temperature=temperature)
+            generated_output = model.generate(
+                tokenized_str,
+                return_dict_in_generate=True,
+                do_sample=False,
+                max_length=start_len + max_new_tokens,
+                output_scores=True,
+                temperature=temperature,
+            )
 
-        # else:        
+        # else:
         except:
-            generated_output = custom_generate(model, tokenized_str, num_new_tokens=max_new_tokens, temperature=temperature, stop_tokens=[tokenizer.eos_token_id])
+            generated_output = custom_generate(
+                model,
+                tokenized_str,
+                num_new_tokens=max_new_tokens,
+                temperature=temperature,
+                stop_tokens=[tokenizer.eos_token_id],
+            )
 
-        tokenized_result = generated_output['sequences'][0]
+        tokenized_result = generated_output["sequences"][0]
         if not include_input:
             tokenized_result = tokenized_result[start_len:]
         # print(tokenized_result)
         if with_logprobs:
             # rows should be token number, columns should be alternating ith token and probability of ith token, fill in with probabilities
             data = []
-            for score in generated_output['scores']:
+            for score in generated_output["scores"]:
                 # a tensor of logits, translate into probabilities
                 probs = torch.nn.functional.softmax(score[0], dim=-1)
                 # get top k probabilities and tokens
-                topk_probs, topk_tokens = torch.topk(probs, top_tokens)            
+                topk_probs, topk_tokens = torch.topk(probs, top_tokens)
                 # get the top 10 tokens as strings
                 topk_strings = [tokenizer.decode(token) for token in topk_tokens]
 
                 row = {}
                 # fill in df
                 for i in range(top_tokens):
-                    row[f'Token_{i+1}'] = topk_tokens[i].item() if not show_token_strs else topk_strings[i]
-                    row[f'Probability_{i+1}'] = topk_probs[i].item()
+                    row[f"Token_{i+1}"] = (
+                        topk_tokens[i].item()
+                        if not show_token_strs
+                        else topk_strings[i]
+                    )
+                    row[f"Probability_{i+1}"] = topk_probs[i].item()
                 data.append(row)
             probs_df = pd.DataFrame(data)
-            
-            return tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(str, ""), probs_df
+
+            return (
+                tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(
+                    str, ""
+                ),
+                probs_df,
+            )
         else:
-            return tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(str, "")
+            return tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(
+                str, ""
+            )
 
-
-    def __init__(self, 
-                 dataset_path=None, 
-                 use_train_data=False,
-                 ):
+    def __init__(
+        self,
+        dataset_path=None,
+        use_train_data=False,
+    ):
         """
         dataset_path is json file that looks like:
         [
@@ -166,18 +214,34 @@ class HPCompletionsFamiliarity(Task):
         if dataset_path is None:
             dataset_path = "tasks/hp/data/msr_data/evaluation_prompts.json"
 
-        with open(dataset_path, 'r') as f:
+        with open(dataset_path, "r") as f:
             self.raw_dataset = json.load(f)
-        assert isinstance(self.raw_dataset, list), "Dataset should be a list of dictionaries"
+        assert isinstance(
+            self.raw_dataset, list
+        ), "Dataset should be a list of dictionaries"
 
-        prompts_references = [(datapoint['prompt']['prompt'], datapoint["prompt"]["references"]) for datapoint in self.raw_dataset]
+        prompts_references = [
+            (datapoint["prompt"]["prompt"], datapoint["prompt"]["references"])
+            for datapoint in self.raw_dataset
+        ]
         self.prompts_references = prompts_references
 
-    
-    def generate_responses(self, model, tokenizer, save_path=None, eval_onthe_fly=True, eval_model='gpt-3.5-turbo', n_questions=None, verbose=False, max_new_tokens=10, max_eval_tokens=1, **kwargs):
+    def generate_responses(
+        self,
+        model,
+        tokenizer,
+        save_path=None,
+        eval_onthe_fly=True,
+        eval_model="gpt-3.5-turbo",
+        n_questions=None,
+        verbose=False,
+        max_new_tokens=10,
+        max_eval_tokens=1,
+        **kwargs,
+    ):
 
         self.answered_dataset = []
-        
+
         # if eval_onthe_fly:
         #     if eval_model is None:
         #         eval_model = 'gpt-3.5-turbo'
@@ -196,59 +260,65 @@ class HPCompletionsFamiliarity(Task):
             prompts_iter = enumerate(self.prompts_references[:n_questions])
 
         for i, (prompt, references) in prompts_iter:
-            results_dict = {
-                'raw_question': prompt
-                }
-            response = self.generate_sentence(prompt, model, tokenizer, max_new_tokens=max_new_tokens, include_input=False, **kwargs).strip()
+            results_dict = {"raw_question": prompt}
+            response = self.generate_sentence(
+                prompt,
+                model,
+                tokenizer,
+                max_new_tokens=max_new_tokens,
+                include_input=False,
+                **kwargs,
+            ).strip()
             # add response to results dict
-            results_dict['completion'] = {
-                'question': prompt,
-                'response': response,
+            results_dict["completion"] = {
+                "question": prompt,
+                "response": response,
                 "references": references,
             }
 
             # run evaluation
             if eval_onthe_fly:
                 model_grade = get_model_grade(
-                    client=client, 
-                    question=prompt, 
+                    client=client,
+                    question=prompt,
                     response=response,
-                    references=references, 
-                    model=eval_model, 
-                    max_tokens=max_eval_tokens
-                    )
-                results_dict['completion']['model_grade'] = model_grade
-                
+                    references=references,
+                    model=eval_model,
+                    max_tokens=max_eval_tokens,
+                )
+                results_dict["completion"]["model_grade"] = model_grade
+
             self.answered_dataset.append(results_dict)
-            
+
             if save_path is not None:
                 save_list_to_jsonl(save_path, self.answered_dataset)
                 # if verbose:
                 #     print(f"Saved results to {save_path}")
 
-    def run_model_evals(self, eval_model='gpt-3.5-turbo', max_eval_tokens=1, save_path=None):
+    def run_model_evals(
+        self, eval_model="gpt-3.5-turbo", max_eval_tokens=1, save_path=None
+    ):
         # if eval_onthe_fly was False, run evals now
         updated_dataset = []
         for i, datapoint in enumerate(self.answered_dataset):
             # model_grade = get_model_grade(client, datapoint['completion']['question'], datapoint['completion']['response'], model=eval_model, max_tokens=max_eval_tokens)
             model_grade = get_model_grade(
-                client=client, 
-                question=datapoint['completion']['question'], 
-                response=datapoint['completion']['response'],
-                references=datapoint['completion']['references'],
-                model=eval_model, 
-                max_tokens=max_eval_tokens
-                )
-            self.answered_dataset[i]['completion']['model_grade'] = model_grade
+                client=client,
+                question=datapoint["completion"]["question"],
+                response=datapoint["completion"]["response"],
+                references=datapoint["completion"]["references"],
+                model=eval_model,
+                max_tokens=max_eval_tokens,
+            )
+            self.answered_dataset[i]["completion"]["model_grade"] = model_grade
             updated_dataset.append(self.answered_dataset[i])
         self.answered_dataset = updated_dataset
         if save_path is not None:
             save_list_to_jsonl(save_path, self.answered_dataset)
 
-
     def get_accuracies(self, question_types=None, results_dataset=None):
         if results_dataset is not None:
-            with open(results_dataset, 'r') as f:
+            with open(results_dataset, "r") as f:
                 lines = f.readlines()
                 self.answered_dataset = [json.loads(line) for line in lines]
         assert self.answered_dataset != [], "Must generate responses first"
@@ -256,7 +326,7 @@ class HPCompletionsFamiliarity(Task):
         assert self.answered_dataset != [], "Must generate responses first"
 
         if question_types is None:
-            question_types = ['completion']
+            question_types = ["completion"]
 
         if isinstance(question_types, str):
             question_types = [question_types]
@@ -266,10 +336,10 @@ class HPCompletionsFamiliarity(Task):
         total_questions = 0
         total_familiarity = 0
 
-        for i, datapoint in enumerate(self.answered_dataset):            
+        for i, datapoint in enumerate(self.answered_dataset):
             for question_type in question_types:
                 # print(f"{datapoint=}, {question_type=}")
-                response = datapoint[question_type]['model_grade']
+                response = datapoint[question_type]["model_grade"]
                 model_responses[response] += 1
 
                 if response == 3:
@@ -282,18 +352,20 @@ class HPCompletionsFamiliarity(Task):
                     if response == 1 or response == 0:
                         total_questions += 1
                     # assert response == '1' or response == '0', f"Model grade should be 0, 1, 2, or 3 but is {response}"
-        return total_familiarity/total_questions, model_responses
+        return total_familiarity / total_questions, model_responses
+
 
 class HPFamiliarityTranchedByBook(HPCompletionsFamiliarity):
 
-    def __init__(self, book_idx:int, *args, **kwargs):
+    def __init__(self, book_idx: int, *args, **kwargs):
 
         script_dir = os.path.dirname(__file__)
-        book_familiarity_path = os.path.join(script_dir, f"data/tranched_by_book/book_{book_idx}_familiarity.json")
-
+        book_familiarity_path = os.path.join(
+            script_dir, f"data/tranched_by_book/book_{book_idx}_familiarity.json"
+        )
 
         super().__init__(
             dataset_path=book_familiarity_path,
-            *args, 
+            *args,
             **kwargs,
-            )
+        )
