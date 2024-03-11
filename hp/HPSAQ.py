d2cba1c13f63ce29340f16ea6fbd62d2742d13a5
aengusl <aenguslynch@gmail.com>
2024-03-11 09:28:53 +0000

    batch it

--- hp/HPSAQ.py
+++ hp/HPSAQ.py
@@ -8,11 +8,12 @@ import os
 from datetime import datetime
 from tasks.task import Task
 from tasks.inference_utils import custom_generate
+from tqdm import tqdm
 
 try:
     openai.api_key = os.getenv("OPENAI_API_KEY")
     client = openai.Client(
-        organization='org-X6T6Ar6geRtOrQgQTQS3OUpw',
+        # organization='org-X6T6Ar6geRtOrQgQTQS3OUpw',
     )
 except:
     print("OpenAI API key not found, will not be able to run evaluations on HPSAQ Task")
@@ -146,6 +147,88 @@ def generate_sentence(str, model, tokenizer, with_logprobs=False, max_new_tokens
     else:
         return tokenizer.decode(tokenized_result, skip_special_tokens=True).replace(str, "")
     
+
+
+def batch_generate_sentence(
+    model,
+    tokenizer,
+    strs: list,  # strs is now a list of strings
+    with_logprobs=False,
+    max_new_tokens=20,
+    top_tokens=5,
+    show_token_strs=True,
+    temperature=1.0,
+    include_input=True,
+    device='cuda'
+):
+    # Encode all the inputs at once
+    tokenizer.padding_side = "left"
+    tokenized_inputs = tokenizer.batch_encode_plus(
+        strs, return_tensors="pt", padding=True,
+    )
+    tokenizer.pad_token = tokenizer.eos_token if tokenizer.pad_token is None else tokenizer.pad_token
+    
+    tokenized_inputs = {k: v.to(device) for k, v in tokenized_inputs.items()}  # Move to model's device
+
+    try:
+        with torch.no_grad():
+            outputs = model.generate(
+                **tokenized_inputs,
+                max_length=tokenized_inputs['input_ids'].shape[1] + max_new_tokens,
+                temperature=temperature,
+                top_k=top_tokens,
+                return_dict_in_generate=True,
+                output_scores=with_logprobs,
+                pad_token_id=tokenizer.pad_token_id,
+            )
+            sequences = outputs.sequences
+            scores = outputs.scores if with_logprobs else None
+
+    except Exception as e:
+
+        print(f"Falling back to custom generation due to exception: {e}\nRunning model as a model inference function instead of a huggingface model.")
+
+        custom_output = custom_generate(
+            model_inference_fn=model,
+            input=tokenized_inputs['input_ids'],
+            num_new_tokens=max_new_tokens,
+            temperature=temperature,
+            stop_tokens=[tokenizer.eos_token_id],
+            verbose=False  # Set to True for progress bar
+        )
+        sequences = custom_output["sequences"]
+        scores = custom_output["scores"]
+
+    # decoded_sentences = [tokenizer.decode(ids, skip_special_tokens=True) for ids in outputs.sequences]
+    decoded_sentences = [tokenizer.decode(ids, skip_special_tokens=True) for ids in sequences]
+
+
+    if not include_input:
+        decoded_sentences = [sentence[len(strs[i]):] for i, sentence in enumerate(decoded_sentences)]
+
+    if with_logprobs and scores is not None:
+        data = []
+        for i, score in enumerate(scores):
+            probs = torch.softmax(score, dim=-1)
+            topk_probs, topk_tokens = probs.topk(top_tokens, dim=-1)
+
+            for batch_index in range(topk_tokens.size(0)):
+                topk_strings = [tokenizer.decode(token.item(), skip_special_tokens=True) for token in topk_tokens[batch_index]]
+                row = {}
+                for j in range(top_tokens):
+                    token_key = f"Token_{j+1}"
+                    prob_key = f"Probability_{j+1}"
+                    row[token_key] = topk_strings[j] if show_token_strs else topk_tokens[batch_index][j].item()
+                    row[prob_key] = topk_probs[batch_index][j].item()
+                data.append(row)
+
+        probs_df = pd.DataFrame(data)
+        return decoded_sentences, probs_df
+    else:
+        return decoded_sentences
+
+
+
 def clear_gpu(model):
     model.cpu()
     torch.cuda.empty_cache()
@@ -242,9 +325,10 @@ class HPSAQ(Task):
 
         self.unrelated_few_shot_question = self.prefix_system_prompt + self.system_prompt + self.suffix_system_prompt + self.unrelated_few_shot_formatted_template + self.suffix_question
 
-    def generate_responses(self, model, tokenizer, save_path=None, eval_onthe_fly=True, question_types=None, eval_model=None, n_questions=None, verbose=True, max_new_tokens=10, **kwargs):
+    def generate_responses(self, model, tokenizer, save_path=None, eval_onthe_fly=True, question_types=None, eval_model=None, n_questions=None, verbose=False, max_new_tokens=10, **kwargs):
 
         # self.format_prompts()
+        tokenizer.pad_token = tokenizer.eos_token if tokenizer.pad_token is None else tokenizer.pad_token
 
         self.answered_dataset = []
 
@@ -323,9 +407,85 @@ class HPSAQ(Task):
             if verbose:
                 print(f"Saved results to {save_path}")
         print(f"Saved results to {save_path}")
+
+    def batch_generate_responses(self, model, tokenizer, batch_size=25, save_path=None, eval_onthe_fly=True, question_types='zero_shot', eval_model=None, n_questions=None, verbose=False, max_new_tokens=10, **kwargs):
+
+        self.answered_dataset = []
+
+        tokenizer.pad_token = tokenizer.eos_token if tokenizer.pad_token is None else tokenizer.pad_token
+
+        if question_types is None:
+            question_types = ['zero_shot', 'few_shot']  # TODO: unrelated few_shot is not working
+        if isinstance(question_types, str):
+            question_types = [question_types]
+        for question_type in question_types:
+            assert question_type in ['zero_shot', 'few_shot', 'unrelated_few_shot'], f"Question type {question_type} not recognized"
+        
+        if eval_onthe_fly:
+            if eval_model is None:
+                eval_model = 'gpt-3.5-turbo'
+
+        if save_path is None:
+            exp_time = datetime.now().strftime("%a-%b%-d-%H%M")
+            os.makedirs('temp', exist_ok=True)
+            save_path = f'temp/{exp_time}.jsonl'
+
+        questions = [datapoint['question'] for datapoint in self.raw_dataset[:n_questions]]
+        formatted_questions = []
+
+        for question in questions:
+            self.zero_shot_formatted_template = self.zero_shot_template.format(question=question)
+            self.few_shot_formatted_template = self.few_shot_template.format(question=question)
+            self.unrelated_few_shot_formatted_template = self.unrelated_few_shot_template.format(question=question)
+
+            self.format_prompts()
+
+            for question_type in question_types:
+                if question_type == 'zero_shot':
+                    formatted_questions.append(self.zero_shot_question)
+                elif question_type == 'few_shot':
+                    formatted_questions.append(self.few_shot_question)
+                elif question_type == 'unrelated_few_shot':
+                    formatted_questions.append(self.unrelated_few_shot_question)
+
+        # Generate responses in batches
+        responses = []
+        for i in tqdm(range(0, len(formatted_questions), batch_size)):
+            responses.append(batch_generate_sentence(
+                model=model, 
+                tokenizer=tokenizer, 
+                strs=formatted_questions[i: i+batch_size], 
+                max_new_tokens=max_new_tokens, 
+                **kwargs)
+                )
+
+        for i, response in enumerate(responses):
+            question_type = question_types[i % len(question_types)]
+            question = questions[i // len(question_types)]
+            true_answer = self.raw_dataset[i // len(question_types)]['true_answer']
+
+            results_dict = {
+                'raw_question': question,
+                'true_answer': true_answer,
+                question_type: {
+                    'question': formatted_questions[i],
+                    'response': response,
+                }
+            }
+
+            if eval_onthe_fly:
+                model_grade = get_model_grade(client, question, response, true_answer, model=eval_model, max_tokens=1)
+                results_dict[question_type]['model_grade'] = model_grade
+
+            self.answered_dataset.append(results_dict)
+
+        save_list_to_jsonl(save_path, self.answered_dataset)
+        if verbose:
+            print(f"Saved results to {save_path}")
+        print(f"Saved results to {save_path}")
                 
 
-    def get_accuracies(self, question_types=None, results_dataset=None):
+    def get_accuracies(self, question_types='zero_shot', results_dataset=None):
 
         if results_dataset is not None:
             with open(results_dataset, 'r') as f:
