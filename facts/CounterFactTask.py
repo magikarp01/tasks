c4c5ccaa4997e3a1fa36ad6dacb047debe46230f
magikarp01 <philliphguo@gmail.com>
2024-09-10 06:17:00 +0000

    adversarial counterfact evals

--- facts/CounterFactTask.py
+++ facts/CounterFactTask.py
@@ -9,6 +9,7 @@ from jaxtyping import Float
 import json
 import einops
 from tqdm.auto import tqdm
+import random
 
 def get_token_sequence_pos(tokenizer, prompt_list, token_strs, batch_size=64):
     
@@ -47,9 +48,9 @@ class CounterFactTask(Task):
         Arguments:
             check_full_answer (bool): Whether to check the full answer or just the first token.
         """
-        assert model_type in ["gemma_7b", "gemma2_9b", "gemma-2"]
-        if model_type == "gemma-2":
-            model_type = "gemma_2_9b"
+        assert model_type in ["gemma_7b", "gemma2_9b", "gemma-2", "gemma_2_9b"]
+        if model_type == "gemma-2" or model_type == "gemma_2_9b":
+            model_type = "gemma2_9b"
         self.batch_size = batch_size
         self.tokenizer = tokenizer
         assert self.tokenizer.padding_side == "right", "Tokenizer should be right-padded for this task"
@@ -209,7 +210,7 @@ def create_icl_mc_format(train_rows):
 
     for row in train_rows:
         # Combine correct and incorrect choices
-        all_choices = [row["target_true"]] + row["targets_false"]
+        all_choices = [row["target_true_mc"]] + row["targets_false_mc"]
         
         # Shuffle choices
         shuffled_indices = list(range(4))
@@ -223,7 +224,7 @@ def create_icl_mc_format(train_rows):
             'response_d': all_choices[shuffled_indices[3]]
         }
 
-        full_icl_format += mc_question_format.format(question=row["question"], **choice_dict) + f" {'ABCD'[shuffled_indices.index(0)]}\n\n"
+        full_icl_format += mc_question_format.format(question=row["question_mc"], **choice_dict) + f" {'ABCD'[shuffled_indices.index(0)]}\n\n"
     return full_icl_format
 
 class CounterFactTask_MC(CounterFactTask):
@@ -238,17 +239,116 @@ class CounterFactTask_MC(CounterFactTask):
         self.test_dataset = Dataset.from_pandas(self.test_df)
         self.set_loaders(self.train_dataset, self.test_dataset, shuffle=shuffle)
     
-    def calculate_loss(self, model, batch):
-        # run prompts
-        labels = self.tokenizer(batch["target_false"], return_tensors="pt", padding=True).input_ids
-        if self.tokenizer.bos_token_id in labels[0]:
-            labels = labels[:, 1]
-        else:
-            labels = labels[:, 0]
-        labels = labels.to(self.device)
-        last_logits = get_final_logits(model, self.tokenizer, batch["prompt"])
-        return self.criterion(last_logits, labels)
+    def __init__(self, batch_size, tokenizer, device="cuda", model_type="gemma_7b", min_prob_threshold=0.5, check_full_answer=False, shuffle=True, criterion="cross_entropy", criterion_kwargs={}, forget_fact_subset=None, train_test_split=True, n_shots=0, is_forget_dataset=None):
+        """
+        Use prefiltered CounterFact dataset (can filter for a given probability threshold).
+        Arguments:
+            check_full_answer (bool): Whether to check the full answer or just the first token.
+        """
+        assert model_type in ["gemma_7b", "gemma2_9b", "gemma-2", "gemma_2_9b"]
+        if model_type == "gemma-2" or model_type == "gemma_2_9b":
+            model_type = "gemma2_9b"
+        self.batch_size = batch_size
+        self.tokenizer = tokenizer
+        assert self.tokenizer.padding_side == "right", "Tokenizer should be right-padded for this task"
+        self.shuffle = shuffle
+        self.counterfact_df = load_dataset("PhillipGuo/counterfact-with-gemma-probs", split=model_type).to_pandas()
+        if min_prob_threshold is not None:
+            self.counterfact_df = self.counterfact_df[self.counterfact_df["prob_of_correct_answer"] > min_prob_threshold]
+        self.check_full_answer = check_full_answer
+        self.device = device
+        if criterion == "cross_entropy":
+            self.criterion = torch.nn.CrossEntropyLoss(**criterion_kwargs)
+        elif criterion == "log_1_minus_p":
+            self.criterion = lambda logits, labels: log_1_minus_p_loss(logits, labels, **criterion_kwargs)
 
+        self.n_shots = n_shots
+        counterfact_mc_df = pd.read_parquet("tasks/facts/data/counterfact_mc_questions.parquet").rename({"target_true": "target_true_mc", "targets_false": "targets_false_mc", "question": "question_mc"}, axis=1)
+        self.counterfact_df = self.counterfact_df.merge(counterfact_mc_df, on="prompt_id", how="inner")
+
+        if is_forget_dataset is not None: 
+            forget_df = self.counterfact_df.copy()
+            if forget_fact_subset is not None: # either int or list of indices
+                if isinstance(forget_fact_subset, int):
+                    forget_fact_subset = forget_df.iloc[:forget_fact_subset].index
+                elif isinstance(forget_fact_subset, list) and isinstance(forget_fact_subset[0], str): # list of prompts
+                    forget_fact_subset = forget_df[forget_df["prompt"].isin(forget_fact_subset)].index
+                # forget_df = forget_df.iloc[forget_fact_subset]
+            
+            if is_forget_dataset:
+                self.counterfact_df = forget_df.iloc[forget_fact_subset]
+                print("Forget dataset with ", len(self.counterfact_df), " examples")
+
+            else:
+                self.counterfact_df = forget_df[~forget_df.index.isin(forget_fact_subset)]
+                print("Maintain dataset with ", len(self.counterfact_df), " examples")
+
+        if train_test_split:
+            self.train_df = self.counterfact_df.iloc[:int(0.8*len(self.counterfact_df))]
+            self.test_df = self.counterfact_df.iloc[int(0.8*len(self.counterfact_df)):]
+        else:
+            self.train_df = self.counterfact_df
+            self.test_df = self.counterfact_df
+
+        self.mc_question_format = mc_question_format
+        if self.n_shots > 0:
+            # must either use shots from train, or be a forget set and use shots from the maintain train set
+            assert is_forget_dataset is not None or train_test_split, "If you want to use ICL, the ICL examples must be from the train set (set train_test_split=True), or if this is a forget task (with no train-test-split), grab the ICL examples from the maintain set."
+            if is_forget_dataset:
+                # grab from forget_df[~forget_df.index.isin(forget_fact_subset)]
+                icl_df = forget_df[~forget_df.index.isin(forget_fact_subset)].iloc[:self.n_shots]
+            else:
+                icl_df = self.train_df.iloc[:self.n_shots]
+            self.mc_question_format = create_icl_mc_format(Dataset.from_pandas(icl_df).to_list()) + self.mc_question_format
+
+        if self.n_shots > 0:
+            # there should be no overlap between the icl_df and the test_df
+            assert len(set(icl_df["prompt_id"]) & set(self.test_df["prompt_id"])) == 0, "There is overlap between the ICL examples and the test set"
+
+        self.train_dataset = Dataset.from_pandas(self.train_df)
+        self.test_dataset = Dataset.from_pandas(self.test_df)
+        self.set_loaders(self.train_dataset, self.test_dataset, shuffle=shuffle)
+    
+    def format_batch(self, batch):
+        all_questions = []
+        correct_labels = []
+        batch_size = len(list(batch.values())[0])
+        for i in range(batch_size):
+            choices = [batch["target_true_mc"][i]] + [batch["targets_false_mc"][j][i] for j in range(len(batch["targets_false_mc"]))]
+            shuffled_indices = list(range(4))
+            random.shuffle(shuffled_indices)
+            correct_labels.append(" " + "ABCD"[shuffled_indices.index(0)])
+            choice_dict = {
+                'response_a': choices[shuffled_indices[0]],
+                'response_b': choices[shuffled_indices[1]],
+                'response_c': choices[shuffled_indices[2]],
+                'response_d': choices[shuffled_indices[3]]
+            }
+            all_questions.append(self.mc_question_format.format(question=batch["question_mc"][i], **choice_dict))
+        return all_questions, correct_labels
+
+
+    def calculate_loss(self, model, batch):
+        """
+        Get the probability of the correct answer, either only the first token or the full answer.
+        I think first token only should be faster because doesn't require syncing.
+        """
+        if self.check_full_answer: # don't think we need this
+            # full_strings = batch["prompt"] + batch["target_true"]
+            # substring_start_positions, substring_end_positions = get_token_sequence_pos(self.tokenizer, full_strings, batch["target_true"])
+            # labels = torch.tensor(substring_end_positions)
+            raise NotImplementedError("Checking full answer not implemented, not needed for gemma")
+        else:
+            # run prompts
+            prompts, labels = self.format_batch(batch)
+            if self.tokenizer.bos_token_id in labels[0]:
+                labels = labels[:, 1]
+            else:
+                labels = labels[:, 0]
+            labels = labels.to(self.device)
+            last_logits = get_final_logits(model, self.tokenizer, prompts)
+            return self.criterion(last_logits, labels)
+    
     def get_test_accuracy(self, model, use_test_data=True, continuous=True, n_iters=1):
         """
         Get the accuracy of the model on the test set.
@@ -257,8 +357,9 @@ class CounterFactTask_MC(CounterFactTask):
             accs = []
             for i in range(n_iters):
                 batch = self.get_batch(train=not use_test_data)
-                last_logits = get_final_logits(model, self.tokenizer, batch["prompt"])
-                labels = self.tokenizer(batch["target_false"], return_tensors="pt", padding=True).input_ids
+                prompts, labels = self.format_batch(batch)
+                last_logits = get_final_logits(model, self.tokenizer, prompts)
+                labels = self.tokenizer(labels, return_tensors="pt", padding=True).input_ids
                 if self.tokenizer.bos_token_id in labels[0]:
                     labels = labels[:, 1]
                 else:
@@ -273,6 +374,64 @@ class CounterFactTask_MC(CounterFactTask):
                     accs.append((preds == labels).sum().item() / len(labels))
             return sum(accs) / len(accs)
 
+from transformers import AutoTokenizer
+from tasks.general_capabilities.MCTask_redo import run_general_evals
+def adversarial_counterfact_eval(model, model_type, batch_size, n_iters=5, continuous=True, 
+                            test_forget_maintain=True, 
+                            task_init_kwargs={},
+                            forget_task_init_kwargs={}, maintain_task_init_kwargs={},
+                            include_evals=["Normal", "MC", "Paraphrase", "Neighborhood", "MMLU"], general_batch_size=5, mc_batch_size=8):
+    if model_type == "gemma-2" or model_type == "gemma_2_9b" or model_type == "gemma2_9b":
+        tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-9b", padding_side="right")  
+    elif model_type == "gemma" or model_type == "gemma_7b" or model_type == "gemma-7b":
+        tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b", padding_side="right")
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    accuracies = {}
+
+    def update_accuracies(eval_type, eval_constructor):
+        if test_forget_maintain:
+            accuracies[eval_type] = {}
+            accuracies[eval_type]["forget"] = 0
+            accuracies[eval_type]["maintain"] = 0
+            # for sport in ["football", "baseball", "basketball"]:
+            #     accuracies[eval_type][sport] = 0
+            #     for i in range(n_iters):
+            #         try:
+            #             temp_task = eval_constructor(batch_size=batch_size, tokenizer=tokenizer, is_forget_dataset=True, forget_sport_subset={sport}, **task_init_kwargs)
+            #         except:
+            #             temp_task = eval_constructor(batch_size=batch_size, tokenizer=tokenizer, is_forget_dataset=True, forget_sport_subset={sport}, **task_init_kwargs)
+            #         accuracies[eval_type][sport] += temp_task.get_test_accuracy(model, continuous=continuous) / n_iters
+            if eval_type == "MC":
+                forget_task = eval_constructor(batch_size=mc_batch_size, tokenizer=tokenizer, n_shots=32, **forget_task_init_kwargs)
+                maintain_task = eval_constructor(batch_size=mc_batch_size, tokenizer=tokenizer, n_shots=32, **maintain_task_init_kwargs)
+            else:
+                forget_task = eval_constructor(batch_size=batch_size, tokenizer=tokenizer, **forget_task_init_kwargs)
+                maintain_task = eval_constructor(batch_size=batch_size, tokenizer=tokenizer, **maintain_task_init_kwargs)
+            for i in range(n_iters):
+                accuracies[eval_type]["forget"] += forget_task.get_test_accuracy(model, continuous=continuous) / n_iters
+                accuracies[eval_type]["maintain"] += maintain_task.get_test_accuracy(model, continuous=continuous) / n_iters
+        else:
+            temp_task = eval_constructor(batch_size=batch_size, tokenizer=tokenizer, **task_init_kwargs)
+            accuracies[eval_type] = 0
+            for i in range(n_iters):
+                accuracies[eval_type] += temp_task.get_test_accuracy(model, continuous=continuous) / n_iters
+    
+    if "Normal" in include_evals:
+        update_accuracies("Normal", CounterFactTask)
+    if "MC" in include_evals:
+        update_accuracies("MC", CounterFactTask_MC)
+    if "Paraphrase" in include_evals:
+        update_accuracies("Paraphrase", CounterFactTask_Paraphrase)
+    if "Neighborhood" in include_evals:
+        update_accuracies("Neighborhood", CounterFactTask_Neighborhood)
+
+    if "MMLU" in include_evals:
+        translated_model_type = {"gemma-2": "gemma-2", "gemma_2_9b": "gemma-2", "gemma-2-9b": "gemma-2", "gemma2_9b": "gemma-2", "gemma2-9b": "gemma-2", "gemma_7b": "gemma", "gemma-7b": "gemma"}[model_type]
+        general_capabilities = run_general_evals(model, model_type=translated_model_type, evals_to_include=["MMLU"], batch_size=general_batch_size)
+        accuracies["general"] = general_capabilities
+    
+    return accuracies
 '''
 Translation Code
 from dotenv import load_dotenv
